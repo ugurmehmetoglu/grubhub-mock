afb69d1b22a6d79ef66475690caaf020debd9e66
Ugur Mehmetoglu <ugurmehmetoglu@Ugurs-MacBook-Air.local>
2020-06-30 08:51:41 -0700

    add imgURL division

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -6,12 +6,12 @@ function Card(props) {
     return (
         <div className="card-template">
             <div>
-                <div className="img"><img src={props.imgURL} /></div>
+                <div className="img"><img src={props.food.imgURL} /></div>
                 <div>
-                    <div className="text-name">{props.name}</div>
+                    <div className="text-name">{props.food.name}</div>
                     <div><img src="https://res.cloudinary.com/grubhub-assets/image/upload/v1577660727/subscriptions/flag_oyyevx.svg" /></div>
-                    <div className="text-deliveryTime">{props.deliveryTime}</div>
-                    <div className="rating">{props.rating}</div>
+                    <div className="text-deliveryTime">{props.food.deliveryTime}</div>
+                    <div className="rating">{props.food.rating}</div>
                 </div>
             </div>
 
